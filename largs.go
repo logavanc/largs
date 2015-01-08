94b545c5e980d3f71181a0a6938e436f86643501
Logan VanCuren <logavanc@gmail.com>
2015-01-07 19:02:54 -0800

    Test of push.

--- largs.go
+++ largs.go
@@ -22,38 +22,5 @@ func New() (d *SystemDevice, err error) {
 		return
 	}
 
-	// d = &SystemDevice{}
-	// d.unitLocation = seed.Location
-	// d.unitName = seed.Name
-	// d.commandOn = seed.CommandOn
-	// d.commandOff = seed.CommandOff
-	// d.claimSeconds = seed.ClaimSeconds
-	// d.executor = seed.Executor
-
-	// if seed.Logger == nil {
-
-	// 	// Lifted straight from https://golang.org/src/pkg/log/log.go
-	// 	d.logger = log.New(
-	// 		os.Stderr,
-	// 		fmt.Sprintf("%s/%s: ", seed.Name, seed.Location),
-	// 		log.LstdFlags)
-
-	// } else {
-	// 	d.logger = seed.Logger
-	// }
-
-	// // Create mutexes.
-	// d.stateLock = new(sync.Mutex)
-
-	// // Synchronize initialized device state.
-	// // powered bool initializes to false, so make the hardware match...
-	// d.setPowerOff()
-
-	// // Launch timer go routines.
-	// go d.claimTimer()
-	// go d.powerTimer()
-
-	// d.logger.Println("New device created.")
-
 	return
 }
